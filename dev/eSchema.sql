3f7cb1836ec655fad0761fc7c2c216448d59246b
Sir Yaw <samappjnr@aol.com>
2020-04-03 16:24:57 -0400

    ...

--- dev/eSchema.sql
+++ dev/eSchema.sql
@@ -14,6 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
@@ -22,4 +23,32 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
+
 );
+
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Legal"), ("Finance"), ("Marketing");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 30000 ,1),  
+("Software Engineer", 80000,2),
+("Lawyer", 100000, 3),
+("Accountant",80000,4),
+("Salesperson", 25000 ,1),
+("Lead Engineer", 120000,2),
+("Legal Team Lead", 110000, 3),
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Delgado", "Samuel", 1),
+("Bilaal", "Garner", 2),
+("Guy", "Loire",3),
+("Shea","Ivan",4);
+("Church", "April",3),
+("Kingston","Alston",1);
+("Velazquez", "Silvia",3),
+("McChalis","Ashlee",3);
